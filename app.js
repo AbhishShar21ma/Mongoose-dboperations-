fe243f32e0f5aa0f60b6157a39a90a10bc8c79b8
AbhishShar21ma <119476697+AbhishShar21ma@users.noreply.github.com>
2023-06-04 19:20:20 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,12 +1,57 @@
 import express from "express";
+import mongoose from "mongoose";
 const app = express();
 const port = 3000;
-import mongoose from "mongoose";
-import connectdb from "db.js";
-const db_url = process.env.db_url || "mongodb://localhost:27017";
 
-connectdb(db_url);
+mongoose
+  .connect("mongodb://localhost:27017/Sample", {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => {
+    console.log("Your Db is connected");
+  })
+  .catch((err) => {
+    console.log(err);
+  });
 
+const student = new mongoose.Schema({
+  name: { type: String, required: true },
+  workout: { type: Boolean },
+  height: { type: Number },
+});
+
+const Student = new mongoose.model("Student", student);
+
+const Stu = new mongoose.model("Stu", student);
+const createDoc = async () => {
+  try {
+    const stuDoc = new Student({
+      name: "Abhishek",
+      workout: true,
+      height: "7",
+    });
+    const re = await stuDoc.save();
+    console.log(re);
+  } catch (error) {
+    console.log(error);
+  }
+};
+createDoc();
+const createStu = async () => {
+  try {
+    const me = new Stu({
+      name: "shek",
+      workout: false,
+      height: "5",
+    });
+    const je = await me.save();
+    console.log(je);
+  } catch (error) {
+    console.log(error);
+  }
+};
+createStu();
 app.get("/", (req, res) => {
   res.send("This is first hello world program");
 });
